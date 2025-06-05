cc06a70fe6001a12ef13fded0335d4c7c019e6b7
Silvan Rüegg <77718946+rueeggsilvan@users.noreply.github.com>
2025-06-05 08:44:47 +0200

    Update index.js
    changed text on main page

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <Layout>
       <h1>Willkommen! 🎉</h1>
-      <p>Schwinger beurteilen.</p>
+      <p>Nutze die Schwingerliste um einen Schwinger auszuwählen, um sein Profil anzusehen und ihn zu bewerten.</p>
       <Link href="/schwinger">
         <button style={{ padding: '10px 20px', backgroundColor: '#0070f3', color: 'white', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
           Zur Schwingerliste
