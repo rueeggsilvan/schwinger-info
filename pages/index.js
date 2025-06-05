734605b9a94474289752c7f327fdb7539337fd88
Silvan Rüegg <77718946+rueeggsilvan@users.noreply.github.com>
2025-06-05 10:24:27 +0200

    Update index.js
    gramatical mistake corrected

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <Layout>
       <h1>Willkommen! 🎉</h1>
-      <p>Nutze die Schwingerliste um einen Schwinger auszuwählen, um sein Profil anzusehen und ihn zu bewerten.</p>
+      <p>Nutze die Schwingerliste um einen Schwinger auszuwählen, sein Profil anzusehen und ihn zu bewerten.</p>
       <Link href="/schwinger">
         <button style={{ padding: '10px 20px', backgroundColor: '#0070f3', color: 'white', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
           Zur Schwingerliste
